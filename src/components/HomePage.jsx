965c6a9c9f52ceea344128e58c4e48a995296f98
AlexisCatrix <alexis.4x.ac@gmail.com>
2021-04-13 14:47:47 +0200

    Move content in App to Homepage component for use router correctly

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,11 +1,41 @@
 import NavBar from "./NavBar";
-import DayNews from "./DayNews";
+import SearchBar from "./SearchBar";
+import PokemonCard from "./PokemonCard";
+import { LoaderContainer, Loading, DisplayCard } from "../AppStyled";
 
-export default function HomePage() {
+export default function HomePage({
+  pagination,
+  searchPokemon,
+  input,
+  setInput,
+  pokemonFound,
+  loading,
+  pokemonData,
+}) {
   return (
     <div>
       <NavBar />
-      <DayNews/>
+      <SearchBar
+        searchPokemon={searchPokemon}
+        input={input}
+        setInput={setInput}
+        pokemonFound={pokemonFound}
+      />
+      {pagination()}
+      <div>
+        {loading ? (
+          <LoaderContainer>
+            <Loading src="https://media.giphy.com/media/3oz8xCQIuyFVM6XH4k/giphy.gif" />
+          </LoaderContainer>
+        ) : (
+          <DisplayCard>
+            {pokemonData.map((pokemonProfil, i) => {
+              return <PokemonCard key={i} pokemonProfil={pokemonProfil} />;
+            })}
+          </DisplayCard>
+        )}
+      </div>
+      {pagination()}
     </div>
   );
 }
