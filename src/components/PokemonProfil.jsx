c50cf99ea830c04db5a177ba153ae174da1b9101
AlexisCatrix <alexis.4x.ac@gmail.com>
2021-04-18 18:25:12 +0200

    Create render of PokemonProfil component

--- src/components/PokemonProfil.jsx
+++ src/components/PokemonProfil.jsx
@@ -1,12 +1,32 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 import NavBar from "./NavBar";
 import SearchBar from "./SearchBar";
+import {
+  Card,
+  CardsContainer,
+  Name,
+  Picture,
+  Skills,
+  AbilitySection,
+  Li,
+} from "./CardStyled";
 
 export default function PokemonProfil({
   pokemonFound,
+  pokemonProperties,
+  fetchPokemonData,
   searchPokemon,
   input,
+  loadingProfil,
   setInput,
 }) {
+  let { pokemon } = useParams();
+
+  useEffect(() => {
+    fetchPokemonData(pokemon);
+  });
+
   return (
     <div>
       <NavBar />
@@ -16,6 +36,31 @@ export default function PokemonProfil({
         setInput={setInput}
         pokemonFound={pokemonFound}
       />
+      {!loadingProfil && (
+        <CardsContainer>
+          <Card>
+            <Name>{pokemonProperties.name}</Name>
+            <Picture
+              src={pokemonProperties.sprites.front_default}
+              alt={pokemonProperties.name}
+            />
+            <Skills>
+              <AbilitySection>Ability </AbilitySection>
+              <div>
+                {pokemonProperties.abilities.map((ability, i) => {
+                  return (
+                    <div key={i}>
+                      <ul>
+                        <Li>{ability.ability.name}</Li>
+                      </ul>
+                    </div>
+                  );
+                })}
+              </div>
+            </Skills>
+          </Card>
+        </CardsContainer>
+      )}
     </div>
   );
 }
